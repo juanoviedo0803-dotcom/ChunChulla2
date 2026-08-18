5142c7e652faa315055484e15acad37461397364
juanoviedo0803-dotcom <juanoviedo0803@gmail.com>
2026-08-18 01:26:44 -0500

    Change trigger level from 400 to 75

--- PrestigeManager.js
+++ PrestigeManager.js
@@ -34,11 +34,11 @@ class PrestigeManager {
 
     console.log(`⛏️ Pico nivel ${level}`)
 
-    if (level >= 400 && !this.triggered) {
+    if (level >= 75 && !this.triggered) {
       this.triggered = true
 
       console.log(
-        "🚀 Nivel 400 alcanzado. Enviando /pp..."
+        "🚀 Nivel 75 alcanzado. Enviando /pp..."
       )
 
       this.bot.chat("/pp")
